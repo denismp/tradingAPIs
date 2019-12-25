6f6a40caf59d48f641700447bb5920ff05362175
denismp <denisputnam@verizon.net>
2019-12-25 14:15:38 -0800

    Added the rest of the code for the exercise.

--- ExcerciseAPI.py
+++ ExcerciseAPI.py
@@ -22,11 +22,28 @@ def kraken():
                                headers={'content-type': 'application/json'})
     return krakenTick.json()['result']['XXBTZUSD']['c'][0]
 
+
 # get last btcusd bid and ask orders from bitstamp orderbook
 # bid
-def bistampOrderBookLastBidPrice():
+def btstampOrderBookLastBidPrice():
     bitStampOrderBookLastBidPrice = requests.get('https://www.bitstamp.net/api/v2/order_book/btcsud/')
-    return bitStampOrderBookLastBidPrice.json()['bids']['0']['0']
+    return bitStampOrderBookLastBidPrice.json()['bids'][0][0]
+
+
+def btstampOrderBookLastBidQuantity():
+    bitStampOrderBookLastBidQuantity = requests.get('https://www.bitstamp.net/api/v2/order_book/btcusd/')
+    return bitStampOrderBookLastBidQuantity.json()['bids'][0][1]
+
+
+# ask
+def btstampOrderBookLastAskPrice():
+    bitStampOrderBookLastAskPrice = requests.get('https://www.bitstamp.net/api/v2/order_book/btcusd/')
+    return bitStampOrderBookLastAskPrice.json()['asks'][1][0]
+
+
+def btstampOrderBookLastAskQuantity():
+    bitStampOrderBookLastAskQuantity = requests.get('https://www.bitstamp.net/api/v2/order_book/btusd')
+    return bitStampOrderBookLastAskQuantity.json()['asks'][1][1]
 
 
 def main():
@@ -37,10 +54,39 @@ def main():
     #    args = parser.parse_args()
 
     #    app.run(host='0.0.0.0', port=args.port)
-    response = btstamp()
-    print(response)
-    response = bitfinex()
-    print(response)
+    # response = btstamp()
+    # print(response)
+    # response = bitfinex()
+    # print(response)
+    while True:
+        btstampUSDLive = float(btstamp())
+        coinbUSDLive = float(coinbase())
+        krakenUSDLive = float(kraken())
+        bitfinexUSDLive = float(bitfinex())
+
+        print(" --- ticker ---")
+        print("Bitstamp Price in USD =", btstampUSDLive)
+        print("Coinbase Price in USD =", coinbUSDLive)
+        print("Kraken Price in USD =", krakenUSDLive)
+        print("Bitfinex Price in USD =", bitfinexUSDLive)
+        print(" ")
+
+        btstampOrderBookLastBidP = float(btstampOrderBookLastBidPrice())
+        btstampOrderBookLastBidQ = float(btstampOrderBookLastBidQuantity())
+        btstampOrderBookLastAskP = float(btstampOrderBookLastAskPrice())
+        btstampOrderBookLastAskQ = float(btstampOrderBookLastAskQuantity())
+
+        print(" --- bitstamp BTCUSD orders ---")
+        print("last bid:")
+        print("         price =", btstampOrderBookLastBidP)
+        print("         quantity =", btstampOrderBookLastBidQ)
+        print("last ask:")
+        print("         price =", btstampOrderBookLastAskP)
+        print("         quantity =", btstampOrderBookLastAskQ)
+        print(" ")
+        print(" ")
+        print(" ")
+        time.sleep(3)  # 120 equals two minutes
 
 
 if __name__ == "__main__":
