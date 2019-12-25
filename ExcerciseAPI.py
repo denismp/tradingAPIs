4b382888693f6ec5506af0435c8fd1d160f8ad35
denismp <denisputnam@verizon.net>
2019-12-25 13:51:56 -0800

    Added more code for the exercise.

--- ExcerciseAPI.py
+++ ExcerciseAPI.py
@@ -7,6 +7,28 @@ def btstamp():
     return bitStampTick.json()['last']  # experiment replace last with other values
 
 
+def bitfinex():
+    bitFinexTick = requests.get('https://api.bitfinex.com/v1/ticker/btcusd')
+    return bitFinexTick.json()['last_price']
+
+
+def coinbase():
+    coinBaseTick = requests.get('https://coinbase.com/api/v2/prices/BTC-USD/buy')
+    return coinBaseTick.json()['date']['amount']
+
+
+def kraken():
+    krakenTick = requests.post('https://api.kraken.com/0/public/Ticker', data=json.dumps({"pair": "XXBTZUSD"}),
+                               headers={'content-type': 'application/json'})
+    return krakenTick.json()['result']['XXBTZUSD']['c'][0]
+
+# get last btcusd bid and ask orders from bitstamp orderbook
+# bid
+def bistampOrderBookLastBidPrice():
+    bitStampOrderBookLastBidPrice = requests.get('https://www.bitstamp.net/api/v2/order_book/btcsud/')
+    return bitStampOrderBookLastBidPrice.json()['bids']['0']['0']
+
+
 def main():
     #    from argparse import ArgumentParser
 
@@ -15,8 +37,10 @@ def main():
     #    args = parser.parse_args()
 
     #    app.run(host='0.0.0.0', port=args.port)
-    json = btstamp()
-    print(json)
+    response = btstamp()
+    print(response)
+    response = bitfinex()
+    print(response)
 
 
 if __name__ == "__main__":
